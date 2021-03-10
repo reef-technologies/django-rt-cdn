d5ebcc61eb64e4c56ac0be9b4bb0c823ed5ecad2
Maciej Lech <maciej.lech@reef.pl>
2021-03-10 18:10:38 +0100

    Use POST for image resizing

--- django_rt_cdn/contrib/ninja/api.py
+++ django_rt_cdn/contrib/ninja/api.py
@@ -1,4 +1,4 @@
-from typing import List, Literal
+from typing import List, Literal, Union
 from urllib.parse import unquote, urlparse
 
 import requests
@@ -13,19 +13,24 @@ from ninja.responses import codes_4xx
 from pydantic import PositiveInt
 from pydantic.networks import HttpUrl
 
-from .renderers import ImageRenderer
 from .security import TokenAuth
 
 
 FormatType = Literal['bmp', 'ico', 'jpeg', 'png', 'tiff', 'webp']
 
 
-api = NinjaAPI(title='RT CDN API', version='1.0.0', auth=TokenAuth(), renderer=ImageRenderer())
+api = NinjaAPI(title='RT CDN API', version='1.0.0', auth=TokenAuth())
 
 
-class Success(Schema):
-    url: HttpUrl
+class ImageIn(Schema):
     origin: HttpUrl
+    width: PositiveInt = None
+    format: FormatType = None
+    force: bool = False
+
+
+class ImageOut(ImageIn):
+    url: HttpUrl
 
 
 class ErrorDetail(Schema):
@@ -35,29 +40,22 @@ class ErrorDetail(Schema):
 
 
 class Error(Schema):
-    detail: List[ErrorDetail]
+    detail: Union[str, List[ErrorDetail]]
 
 
-def default_request_handler(request, origin, **kwargs):
-    kwargs.setdefault('timeout', 5.0)
-    return requests.get(origin, **kwargs)
+@api.exception_handler(Exception)
+def exception_handler(request, exc):
+    return api.create_response(request, {'detail': str(exc)}, status=500)
 
 
-@api.get('/image', tags=['Image processing'], response={200: Success, codes_4xx: Error})
-def image(
-    request,
-    origin: HttpUrl,
-    width: PositiveInt = None,
-    format: FormatType = None,
-    force: bool = False,
-):
-    request_handler = getattr(settings, 'CDN_IMAGE_REQUEST_HANDLER', None)
-    if request_handler is not None:
-        request_handler_fn = get_by_qname(request_handler, 'request handler')
-    else:
-        request_handler_fn = default_request_handler
+@api.post('/images', tags=['Images'], response={200: ImageOut, codes_4xx: Error})
+def images(request, image_in: ImageIn):
+    image_input_processor_func = getattr(settings, 'CDN_IMAGE_INPUT_PROCESSOR', None)
+    if image_input_processor_func is not None:
+        get_by_qname(image_input_processor_func, 'image input processor')(image_in)
 
-    response = request_handler_fn(request, origin=origin)
+    kwargs = getattr(settings, 'CDN_IMAGE_REQUEST_ARGS', {'timeout': 5.0})
+    response = requests.get(image_in.origin, **kwargs)
 
     try:
         response.raise_for_status()
@@ -78,24 +76,35 @@ def image(
     content = response.content
 
     with ContentFile(content, name=name) as image_fd:
-        if width is None:
+        if image_in.width is None:
             generator = generator_registry.get(
                 'cdn:origin_resolution',
                 source=image_fd,
-                format=format,
+                format=image_in.format,
             )
         else:
             generator = generator_registry.get(
                 'cdn:thumbnail',
                 source=image_fd,
-                width=width,
-                format=format,
+                width=image_in.width,
+                format=image_in.format,
                 crop=False,
                 upscale=False,
             )
 
         file = ImageCacheFile(generator)
-        file.generate(force=force)
-        setattr(request, 'image_file', file)
+        file.generate(force=image_in.force)
 
-    return {'url': file.url, 'origin': response.url}
+    image_out = ImageOut(
+        url=file.url,
+        origin=response.url,
+        width=image_in.width,
+        format=image_in.format,
+        force=image_in.force,
+    )
+
+    image_output_processor_func = getattr(settings, 'CDN_IMAGE_OUTPUT_PROCESSOR', None)
+    if image_output_processor_func is not None:
+        get_by_qname(image_output_processor_func, 'image output processor')(image_out)
+
+    return image_out
